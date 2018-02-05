b79a0bacdc3079e7be907ff640fdcc29dd1871ff
Geoffrey Hart <geoffreylhart@gmail.com>
2018-02-04 20:29:01 -0900

    Adding a basic cube mesh and camera controls

--- Zenith/Game1.cs
+++ Zenith/Game1.cs
@@ -1,82 +1,91 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using Zenith.PrimitiveBuilder;
 
 namespace Zenith
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        
+        VertexIndiceBuffer box;
+        double cameraRotX = 0;
+        double cameraRotY = 0;
+        static double CAMERA_ROT_VEL = 0.01;
+
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            box = CubeBuilder.MakeBasicCube(GraphicsDevice);
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
                 Exit();
+            }
 
-            // TODO: Add your update logic here
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                cameraRotX -= CAMERA_ROT_VEL;
+            }
 
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                cameraRotX += CAMERA_ROT_VEL;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                cameraRotY -= CAMERA_ROT_VEL;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                cameraRotY += CAMERA_ROT_VEL;
+            }
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
-
+            GraphicsDevice.Clear(Color.Black);
+            var basicEffect3 = new BasicEffect(GraphicsDevice);
+            basicEffect3.LightingEnabled = true;
+            basicEffect3.DirectionalLight0.Direction = new Vector3(-1, 0, 0);
+            basicEffect3.DirectionalLight0.DiffuseColor = new Vector3(1, 1, 1);
+            basicEffect3.AmbientLightColor = new Vector3(0.2f, 0.2f, 0.2f);
+            //basicEffect3.AmbientLightColor = new Vector3(1, 1, 1);
+            basicEffect3.World = Matrix.CreateRotationX((float)cameraRotY) * Matrix.CreateRotationZ((float)cameraRotX);
+            //basicEffect3.World = Matrix.Identity;
+            basicEffect3.View = Matrix.CreateLookAt(new Vector3(0, -20, 0), new Vector3(0, 0, 0), Vector3.UnitZ); // we'll match Blender for "up" of camera and camera starting position
+            basicEffect3.Projection = Matrix.CreatePerspectiveFieldOfView((float)(Math.PI / 4), 800f / 480f, 0.1f, 100f);
+            foreach (EffectPass pass in basicEffect3.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                GraphicsDevice.Indices = box.indices;
+                GraphicsDevice.SetVertexBuffer(box.vertices);
+                GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0, box.indices.IndexCount / 3);
+            }
             base.Draw(gameTime);
         }
     }
---
+++ Zenith/PrimitiveBuilder/CubeBuilder.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zenith.PrimitiveBuilder
+{
+    internal class CubeBuilder
+    {
+        // according to Blender front=-y, back=y, left=-x, right=x, up=z, down=-z
+        internal static VertexIndiceBuffer MakeBasicCube(GraphicsDevice graphicsDevice)
+        {
+            VertexIndiceBuffer buffer = new VertexIndiceBuffer();
+
+            List<VertexPositionNormalTexture> vertices = new List<VertexPositionNormalTexture>();
+            List<int> indices = new List<int>();
+            for (int i = 0; i < 8; i++)
+            {
+                Vector3 position = new Vector3(i < 4 ? -1 : 1, i % 4 < 2 ? -1 : 1, i % 2 < 1 ? -1 : 1);
+                Vector3 normal = new Vector3(true?-1:1, true ? -1 : 1, true ? -1 : 1);
+                Vector2 tex = new Vector2(0, 0); // don't care
+                //vertices.Add(new VertexPositionColor(position, Color.Green));
+                vertices.Add(new VertexPositionNormalTexture(position, position, tex));
+            }
+            // front face, triangles diagonal goes from bottom left to top right, triangles are formed by going clockwise to make them visible
+            // all the other squares triangles share rotational symmetry (sort of) with this first one (basically copy-paste the first square and then rotate it around the origin purely with the x-axis or z-axis)
+            indices.Add(0);
+            indices.Add(1);
+            indices.Add(5);
+            indices.Add(0);
+            indices.Add(5);
+            indices.Add(4);
+            // back face
+            indices.Add(3);
+            indices.Add(2);
+            indices.Add(6);
+            indices.Add(3);
+            indices.Add(6);
+            indices.Add(7);
+            // right face
+            indices.Add(4);
+            indices.Add(5);
+            indices.Add(7);
+            indices.Add(4);
+            indices.Add(7);
+            indices.Add(6);
+            // left face
+            indices.Add(2);
+            indices.Add(3);
+            indices.Add(1);
+            indices.Add(2);
+            indices.Add(1);
+            indices.Add(0);
+            // top face
+            indices.Add(1);
+            indices.Add(3);
+            indices.Add(7);
+            indices.Add(1);
+            indices.Add(7);
+            indices.Add(5);
+            // bottom face
+            indices.Add(2);
+            indices.Add(0);
+            indices.Add(4);
+            indices.Add(2);
+            indices.Add(4);
+            indices.Add(6);
+            // TODO: change back to BufferUsage.WriteOnly if applicable
+            buffer.vertices = new VertexBuffer(graphicsDevice, VertexPositionNormalTexture.VertexDeclaration, vertices.Count, BufferUsage.None);
+            buffer.vertices.SetData(vertices.ToArray());
+            buffer.indices = new IndexBuffer(graphicsDevice, IndexElementSize.ThirtyTwoBits, indices.Count, BufferUsage.None);
+            buffer.indices.SetData(indices.ToArray());
+            return buffer;
+        }
+    }
+}
