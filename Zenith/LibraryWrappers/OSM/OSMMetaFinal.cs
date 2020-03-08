701b14c84af36ed8e4b7b5ec9752817ab9df831f
Geoffrey Hart <geoffreylhart@gmail.com>
2020-03-07 15:34:14 -0900

    - add even more preprocessing data for us to work with - also start on actually handling big coast

---
+++ Zenith/LibraryWrappers/OSM/OSMMetaFinal.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Zenith.ZGeom;
+using Zenith.ZMath;
+using static Zenith.LibraryWrappers.OSM.OSMMetaManager;
+
+namespace Zenith.LibraryWrappers.OSM
+{
+    // meant to be a more compressed version of OSMMetaManager (no extra data)
+    // TODO: just occurred to me that none of this can handle malformed relations that dont even form a complete shape
+    // TODO: also just occurred to me that maybe our way inits shouldn't use a dictionary, since there could be duplicates??
+    class OSMMetaFinal
+    {
+        public static List<string> naturalTypes = new List<string>() {
+            "coastline", "water", "wood", "scrub", "heath", "moor", "grassland", "fell", "bare_rock", "scree",
+            "shingle", "sand", "mud", "wetland", "glacier", "bay", "strait", "beach", "reef", "dune",
+            "cliff", "isthmus", "peninsula", "rock", "sinkhole", "cave_entrance", "desert"
+        };
+
+        Dictionary<ISector, GridPointInfo[,]> gridPoints; // the final actual info
+        Dictionary<ISector, GridPointInfo[,]> gridTops; // intermediate
+        Dictionary<ISector, GridPointInfo[,]> gridLefts; // intermediate
+
+        internal void LoadAll(string fileName)
+        {
+            OSMMetaManager manager = new OSMMetaManager();
+            manager.LoadAll(fileName);
+            // init
+            gridPoints = new Dictionary<ISector, GridPointInfo[,]>();
+            gridTops = new Dictionary<ISector, GridPointInfo[,]>();
+            gridLefts = new Dictionary<ISector, GridPointInfo[,]>();
+            foreach (var root in ZCoords.GetSectorManager().GetTopmostOSMSectors())
+            {
+                GridPointInfo[,] gp = new GridPointInfo[257, 257];
+                GridPointInfo[,] gt = new GridPointInfo[256, 257];
+                GridPointInfo[,] gl = new GridPointInfo[257, 256];
+                for (int x = 0; x < 257; x++)
+                {
+                    for (int y = 0; y < 257; y++)
+                    {
+                        gp[x, y] = new GridPointInfo();
+                        if (x < 256) gt[x, y] = new GridPointInfo();
+                        if (y < 256) gl[x, y] = new GridPointInfo();
+                    }
+                }
+                gridPoints[root] = gp;
+                gridTops[root] = gt;
+                gridLefts[root] = gl;
+            }
+            foreach (var edge in manager.edgeInfo)
+            {
+                // coastline only, to start with
+                if (manager.wayInfo[edge.wayID].keyValues.ContainsKey("natural") && manager.wayInfo[edge.wayID].keyValues["natural"].Equals("coastline"))
+                {
+                    foreach (var root in ZCoords.GetSectorManager().GetTopmostOSMSectors())
+                    {
+                        var local1 = root.ProjectToLocalCoordinates(edge.longLat1.ToSphereVector());
+                        var local2 = root.ProjectToLocalCoordinates(edge.longLat1.ToSphereVector());
+                        // first, order by X
+                        if (local1.X > local2.X)
+                        {
+                            var temp = local2;
+                            local2 = local1;
+                            local1 = temp;
+                        }
+                        for (int x = (int)Math.Ceiling(local1.X * 256); x < local2.X * 256; x++)
+                        {
+                            double t = (x - local1.X * 256) / (local2.X - local1.X);
+                            int y = (int)(local1.Y + t * (local2.Y - local1.Y));
+                            if (x >= 0 && x < 257 && y >= 0 && y < 256)
+                            {
+                                gridLefts[root][x, y].naturalTypes.Add(0);
+                            }
+                        }
+                        // now, order by Y
+                        if (local1.Y > local2.Y)
+                        {
+                            var temp = local2;
+                            local2 = local1;
+                            local1 = temp;
+                        }
+                        for (int y = (int)Math.Ceiling(local1.Y * 256); y < local2.Y * 256; y++)
+                        {
+                            double t = (y - local1.Y * 256) / (local2.Y - local1.Y);
+                            int x = (int)(local1.X + t * (local2.X - local1.X));
+                            if (x >= 0 && x < 256 && y >= 0 && y < 257)
+                            {
+                                gridTops[root][x, y].naturalTypes.Add(0);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // as a point, represents the land types and relations that contain it
+        // as an edge, represents the same states that will be added or removed upon crossing
+        public class GridPointInfo
+        {
+            public HashSet<long> relations;
+            public HashSet<int> naturalTypes; // by index
+        }
+    }
+}
--- Zenith/LibraryWrappers/OSM/OSMMetaManager.cs
+++ Zenith/LibraryWrappers/OSM/OSMMetaManager.cs
@@ -9,11 +9,11 @@ using Zenith.ZMath;
 namespace Zenith.LibraryWrappers.OSM
 {
     // load, save, and process quick lookups for massive items
-    class OSMMetaManager
+    public class OSMMetaManager
     {
-        HashSet<EdgeInfo> edgeInfo = new HashSet<EdgeInfo>();
-        HashSet<WayInfo> wayInfo = new HashSet<WayInfo>();
-        HashSet<RelationInfo> relationInfo = new HashSet<RelationInfo>();
+        public HashSet<EdgeInfo> edgeInfo = new HashSet<EdgeInfo>();
+        public Dictionary<long, WayInfo> wayInfo = new Dictionary<long, WayInfo>();
+        public Dictionary<long, RelationInfo> relationInfo = new Dictionary<long, RelationInfo>();
 
         internal void SaveAll(string fileName)
         {
@@ -34,9 +34,11 @@ namespace Zenith.LibraryWrappers.OSM
                         bw.Write(e.longLat2.Y);
                     }
                     bw.Write(wayInfo.Count);
-                    foreach (var w in wayInfo)
+                    foreach (var w in wayInfo.Values)
                     {
                         bw.Write(w.id);
+                        bw.Write(w.startNode);
+                        bw.Write(w.endNode);
                         bw.Write(w.keyValues.Count);
                         foreach (var pair in w.keyValues)
                         {
@@ -45,7 +47,7 @@ namespace Zenith.LibraryWrappers.OSM
                         }
                     }
                     bw.Write(relationInfo.Count);
-                    foreach (var r in relationInfo)
+                    foreach (var r in relationInfo.Values)
                     {
                         bw.Write(r.id);
                         bw.Write(r.keyValues.Count);
@@ -88,13 +90,15 @@ namespace Zenith.LibraryWrappers.OSM
                     {
                         WayInfo w = new WayInfo();
                         w.id = br.ReadInt64();
+                        w.startNode = br.ReadInt64();
+                        w.endNode = br.ReadInt64();
                         int keyValueCount = br.ReadInt32();
                         w.keyValues = new Dictionary<string, string>();
                         for (int k = 0; k < keyValueCount; k++)
                         {
                             w.keyValues[br.ReadString()] = br.ReadString();
                         }
-                        if (!wayInfo.Contains(w)) wayInfo.Add(w);
+                        if (!wayInfo.ContainsKey(w.id)) wayInfo[w.id] = w;
                     }
                     int relationInfoCount = br.ReadInt32();
                     for (int j = 0; j < relationInfoCount; j++)
@@ -116,13 +120,13 @@ namespace Zenith.LibraryWrappers.OSM
                         int typesCount = br.ReadInt32();
                         r.types = new List<int>();
                         for (int k = 0; k < typesCount; k++) r.types.Add(br.ReadInt32());
-                        if (!relationInfo.Contains(r)) relationInfo.Add(r);
+                        // if (!relationInfo.ContainsKey(r.id)) relationInfo[r.id] = r; // disabling to save some memory, sure
                     }
                 }
             }
         }
 
-        // took 20.178 hours to save 1.28 GB
+        // took 21.117584 hours to save 4.19 GB
         // saved to 6 different files in case it crashes
         public void LoadAllDetailsFromSource()
         {
@@ -156,8 +160,8 @@ namespace Zenith.LibraryWrappers.OSM
         private void Clear()
         {
             edgeInfo = new HashSet<EdgeInfo>();
-            wayInfo = new HashSet<WayInfo>();
-            relationInfo = new HashSet<RelationInfo>();
+            wayInfo = new Dictionary<long, WayInfo>();
+            relationInfo = new Dictionary<long, RelationInfo>();
         }
 
         private void LoadDetailsFromSource(ISector sector)
@@ -180,21 +184,29 @@ namespace Zenith.LibraryWrappers.OSM
                     {
                         var e = new EdgeInfo() { wayID = way.id, node1 = ref1, node2 = ref2, longLat1 = longLat1, longLat2 = longLat2 };
                         if (!edgeInfo.Contains(e)) edgeInfo.Add(e);
-                        var w = new WayInfo() { id = way.id, keyValues = way.keyValues };
-                        if (!wayInfo.Contains(w)) wayInfo.Add(w);
+                        var w = new WayInfo() { id = way.id, keyValues = way.keyValues, startNode = way.refs.First(), endNode = way.refs.Last() };
+                        if (!wayInfo.ContainsKey(w.id)) wayInfo[w.id] = w;
                     }
                 }
             }
+            HashSet<long> extraWays = new HashSet<long>();
             foreach (var relation in blobs.EnumerateRelations())
             {
                 foreach (var way in relation.memids)
                 {
-                    var r = new RelationInfo() { id = relation.id, keyValues = relation.keyValues, roleValues = relation.roleValues, memids = relation.memids, types = relation.types };
-                    if (wayInfo.Contains(new WayInfo() { id = way }) && !relationInfo.Contains(r))
+                    extraWays.Add(way);
+                    if (wayInfo.ContainsKey(way) && !relationInfo.ContainsKey(relation.id))
                     {
-                        relationInfo.Add(r);
+                        var r = new RelationInfo() { id = relation.id, keyValues = relation.keyValues, roleValues = relation.roleValues, memids = relation.memids, types = relation.types };
+                        relationInfo[r.id] = r;
                     }
                 }
+            }
+            foreach (var way in blobs.EnumerateWays(false))
+            {
+                if (!extraWays.Contains(way.id)) continue;
+                var w = new WayInfo() { id = way.id, keyValues = way.keyValues, startNode = way.refs.First(), endNode = way.refs.Last() };
+                if (!wayInfo.ContainsKey(w.id)) wayInfo[w.id] = w;
             }
         }
 
@@ -239,6 +251,8 @@ namespace Zenith.LibraryWrappers.OSM
         public class WayInfo
         {
             public long id;
+            public long startNode;
+            public long endNode;
             public Dictionary<string, string> keyValues;
 
             public override int GetHashCode()
